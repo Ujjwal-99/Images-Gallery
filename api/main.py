22ec74c0faae87a5cdc45287c473cf9e7be240c7
ujjwal bhatt <ujjwalbhatt180180@gmail.com>
2024-03-08 17:19:06 +0530

    connect front with back using cors

--- api/main.py
+++ api/main.py
@@ -2,16 +2,19 @@ from flask import Flask, request
 import requests
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random"
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY","")
+DEBUG=bool(os.environ.get("DEBUG",True))
 
 if not UNSPLASH_KEY:
    raise EnvironmentError("Please create .env.local file and insery there UNSPLASH_KEY")
 
 app= Flask(__name__)
+CORS(app)
 
 @app.route("/new-image")
 def new_image():
@@ -29,4 +32,4 @@ def new_image():
 
 
 if __name__ == "__main__" :
-  app.run(debug=True) 
+  app.run(debug=DEBUG) 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Search from "./components/Search";
 import ImageCard from "./components/ImageCard";
 import Welcome from "./components/Welcome";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
 
 function App() {
   const [word, setWord] = useState("");
@@ -12,9 +12,7 @@ function App() {
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
     // console.log(word);
-    await fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
-    )
+    await fetch(`${API_URL}/new-image?query=${word}`)
       .then((res) => res.json())
       .then((data) => {
         const newData = [{ ...data, title: word }, ...images];
