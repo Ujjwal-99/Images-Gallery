55a19f273b26833b99ea309358c43d44f962b1f4
ujjwal bhatt <ujjwalbhatt180180@gmail.com>
2024-03-09 16:10:50 +0530

    docker file for api

--- api/main.py
+++ api/main.py
@@ -30,6 +30,10 @@ def new_image():
     data=response.json()
     return data
 
+# @app.route('/images',method=["GET","POST"])
+# def method_name():
+#     pass
+
 
 if __name__ == "__main__" :
   app.run(debug=DEBUG) 
